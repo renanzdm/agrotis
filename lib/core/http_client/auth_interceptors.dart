af024efc5549ea78ebc9135be1d24eceadef1c1c
renanzdm <renanzdm@gmail.com>
2022-05-28 16:43:55 -0300

    .

--- lib/core/http_client/auth_interceptors.dart
+++ lib/core/http_client/auth_interceptors.dart
@@ -4,7 +4,8 @@ class AuthInterceptors extends InterceptorsWrapper {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     String apiKey = const String.fromEnvironment('DEFINE_API_KEY');
-    options.headers['Authorization'] = 'Client-ID $apiKey';
+    options.headers['Authorization'] =
+        'Client-ID UlMjPf4J0GaJq8r-JUPoqzeJWApOYWu4BHCvd8gOg0c';
     handler.next(options);
   }
 }
